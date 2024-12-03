1dd6c5c304b420b97d1ed14c0a012048bf7f5587
Perm <wannuwatbuayangyuen.gmail.com>
2024-12-03 17:31:35 +0700

    uncompleted

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,14 @@
+char before(char x){
+	//Write your function definition here
+	if(x > 65){
+		cout << x-1;
+	}
+	if(x <= 90){
+		cout << x-1;
+	}
+	if(x == 65){
+		cout << x;
+	}
+	else{
+		cout << 0
+	}
